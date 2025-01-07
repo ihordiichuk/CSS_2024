a1e18c7ee4f5b1ac84d850c9dac2cb15037bf9ed
Ihor Hordiichuk <lordwayden@gmail.com>
2025-01-07 13:35:12 +0200

    Fix for buttons with js

--- shared.js
+++ shared.js
@@ -1,25 +1,31 @@
-var backdrop = document.querySelector('.backdrop');
-var modal = document.querySelector('.modal');
-var modalNoButton = document.querySelector('.modal__action--negative');
-var selectPlanButton = document.querySelectorAll('.plan Button');
-var toggleButton = document.querySelector('.toggle-button');
-var mobileNav = document.querySelector('.mobile-nav');
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButton = document.querySelectorAll(".plan Button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButton.length; i++) {
-    selectPlanButton[i].addEventListener('click', function () {
-       selectPlanButton[i].addEventListener('click', function () {
-            modal.style.display = 'block';
-            backdrop.style.display = 'block';
-    });
+  selectPlanButton[i].addEventListener("click", function () {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
 }
 
-backdrop.addEventListener('click', сloseModal);
+backdrop.addEventListener("click", function () {
+  mobileNav.style.display = "block";
+  closeModal();
+});
 
-modalNoButton.addEventListener('cllick', сloseModal);
-
+modalNoButton.addEventListener("cllick", сloseModal);
+modalNoButton.addEventListener("click", closeModal);
 function сloseModal() {
-  backdrop.style.display = 'none';
-  modal.style.display = 'none';
+  backdrop.style.display = "none";
+  modal.style.display = "none";
 }
 
+toggleButton.addEventListener("click", function () {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+});
